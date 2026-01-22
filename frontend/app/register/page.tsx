852915ca99a9ee49507126e7660a78d579b1df4e
Alexiiis <okalexiiis@gmail.com>
2026-01-22 16:38:48 -0700

    Style pages with Gruvbox theme and improve aesthetics

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
-import { LoginForm } from "./components/LoginForm";
+import { RegisterForm } from "./components/RegisterForm";
 
-export default function Login() {
+export default function Register() {
   return (
     <div className="w-full min-h-screen flex items-center justify-center bg-background">
       <section className="w-full max-w-4xl min-h-96 border-2 border-background-secondary flex items-center justify-center bg-background-secondary shadow-lg">
         <div className="space-y-6 w-full max-w-md">
-          <h1 className="text-2xl font-bold text-center text-foreground">Login to Random Chat</h1>
-          <LoginForm />
+          <h1 className="text-2xl font-bold text-center text-foreground">Register for Random Chat</h1>
+          <RegisterForm />
           <p className="text-center text-foreground">
-            Don’t have an account? <Link href="/register" className="text-green hover:text-green-muted transition-colors">Register</Link>
+            Already have an account? <Link href="/login" className="text-green hover:text-green-muted transition-colors">Login</Link>
           </p>
         </div>
       </section>
