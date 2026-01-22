b7eeee1666e0af81bea49326c626c3c4ab284fd7
Alexiiis <okalexiiis@gmail.com>
2026-01-22 16:38:46 -0700

    Improve login error specificity: User not found vs Invalid password

--- backend/src/modules/users/domain/errors/InvalidCredentials.ts
+++ backend/src/modules/users/domain/errors/InvalidCredentials.ts
@@ -1,8 +1,9 @@
 //src/modules/users/domain/errors/InvalidCredentials.ts
+import { UnauthorizedError } from "@core/types/Errors";
 
-export class InvalidCredentials extends Error {
+export class InvalidCredentials extends UnauthorizedError {
   constructor() {
-    super("Invalid credentials");
+    super("Invalid password");
     this.name = "InvalidCredentials";
   }
 }
--- backend/src/modules/users/domain/errors/UserNotFound.ts
+++ backend/src/modules/users/domain/errors/UserNotFound.ts
@@ -1,6 +1,7 @@
 //src/modules/users/domain/errors/UserNotFound.ts
+import { NotFoundError } from "@core/types/Errors";
 
-export class UserNotFound extends Error {
+export class UserNotFound extends NotFoundError {
   constructor() {
     super("User not found");
     this.name = "UserNotFound";
