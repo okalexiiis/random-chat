c1d2772a75ddb5c3a829e6661a4467db15dd212b
okalexiiis <okalexiiis@gmail.com>
2026-01-22 14:05:15 -0700

    Frontend

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,10 @@
+"use-client"
+export default function Login() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <section className="w-full max-w-4xl min-h-96 border-2 border-foreground flex items-center justify-center">
+        login
+      </section>
+    </div>
+  )
+}
